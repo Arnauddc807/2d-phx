c9439e6cfb5c1ec60310faa151a90076ce81e88d
Arnauddc807 <91956139+Arnauddc807@users.noreply.github.com>
2022-09-11 01:08:34 +0200

    more balls

--- code.js
+++ code.js
@@ -5,24 +5,28 @@ class Vector {
     }
 
     add(vec) {
-        this.x += vec.x
-        this.y += vec.y
-        return new Vector(this.x, this.y)
+        return new Vector(this.x + vec.x, this.y + vec.y)
+    }
+    sub(vec) {
+        return new Vector(this.x - vec.x, this.y - vec.y)
     }
     mult(a) {
-        this.x *= a
-        this.y *= a
-        return new Vector(this.x, this.y)
+        return new Vector(this.x * a, this.y * a)
     }
     div(a) {
-        this.x /= a
-        this.y /= a
-        return new Vector(this.x, this.y)
+        return new Vector(this.x / a, this.y / a)
     }
     size() {
       return Math.sqrt(this.x*this.x + this.y*this.y)
     }
-}
+    unit() {
+      if (this.size() == 0) {return new Vector (0, 0)}
+      return new Vector(this.x/this.size(), this.y/this.size())
+    }
+    dot(vec) {
+      return (this.x*vec.x + this.y*vec.y)
+    }
+} 
 
 class Circle {
     constructor(x, y, r, c) {
@@ -35,7 +39,7 @@ class Circle {
         this.velocity = new Vector(0, 0)
         this.position = new Vector(x, y)
 
-
+        this.player = false
     }
 
     drawCircle() {
@@ -49,12 +53,11 @@ class Circle {
     simulate(dt = 2) {
         //integrate physics
         //linear 
-        this.forces = new Vector(horizontal, vertical)
+        
         this.acceleration = this.forces.div(this.mass).mult(dt)
-        this.velocity.add(this.acceleration).mult(dt)
-        this.velocity = this.velocity.mult(0.99)
-        if (this.velocity.size() < 0.1) { this.velocity = new Vector(0, 0) }        
-        this.position.add(this.velocity).mult(dt)
+        this.velocity = this.velocity.add(this.acceleration).mult(1-friction)
+        if (this.acceleration.size == 0 && this.velocity.size < 0.1) {this.velocity = new Vector(0, 0)}      
+        this.position = this.position.add(this.velocity.mult(dt))
 	      this.position.x = ((this.position.x % 800) + 800) % 800
 	      this.position.y = ((this.position.y % 800) + 800) % 800
         this.forces = new Vector(0, 0)
@@ -69,32 +72,14 @@ class Circle {
         */
     }
 
+    user_input() {
+    this.forces = new Vector(horizontal, vertical)
+    }
+
+
+
+
 }
-
-
-
-//start program
-m = document.getElementById("canvas").getContext('2d')
-let friction = 0.1
-c1 = new Circle(400, 400, 25, "blue")
-c2 = new Circle(300, 400, 20, "green")
-hud = new Circle(700, 700, 30, "white")
-
-//keyboard controls
-let leftHeld = false
-let rightHeld = false
-let upHeld = false
-let downHeld = false
-
-/*
-//vehicle controls
-FUTURE CODE
-let steering = 0 //-1 is left 0 is center 1 is right
-let throttle = 0 //0 is coasting, 1 is full throttle
-let brakes = 0 //0 is no brakes, 1 is full brakes
-*/
-let horizontal = 0 // -1 is left 1 is right
-let vertical = 0 // -1 is up 1 is down
 
 //get keypresses down
 document.addEventListener("keydown", (e) => {  
@@ -142,10 +127,39 @@ function Hud() {
 
   m.beginPath();
   m.moveTo(hud.position.x, hud.position.y);
-  m.lineTo(hud.position.x+c1.velocity.x, hud.position.y+c1.velocity.y);
+  m.lineTo(hud.position.x+BALLS[0].velocity.x, hud.position.y+BALLS[0].velocity.y);
   m.strokeStyle = "red";
   m.stroke();
 
+}
+
+
+
+function collision_detection(c1, c2) {
+  if (c1.position.sub(c2.position).size() <= c1.r + c2.r) {
+    return true
+  }
+  return false
+}
+
+function penetration(c1, c2) {
+
+  dist = c1.position.sub(c2.position)
+  dept = c1.r + c2.r - dist.size()
+  res = dist.unit()
+
+  c1.position = c1.position.add(res)
+  c2.position = c2.position.add(res.mult(-1))
+}
+
+function response(c1, c2) {
+  normal = c1.position.sub(c2.position).unit()
+  relVelocity = c1.velocity.sub(c2.velocity)
+  sepVelocity = relVelocity.dot(normal) * elasticity
+  sepVelocityVec = normal.mult(-sepVelocity)
+
+  c1.velocity = c1.velocity.add(sepVelocityVec)
+  c2.velocity = c2.velocity.add(sepVelocityVec.mult(-1))
 }
 
 //keyboard input
@@ -170,6 +184,63 @@ function InputHandler() {
 }
 
 
+function TotalEnergy() {
+  result = 0
+  for (let i = 0; i < BALLS.length; i++) {
+    result += BALLS[i].velocity.size()
+  }
+  m.font = "20px Arial";
+  m.fillStyle = "white";
+  rounded = Math.round(result*100)/100
+  m.fillText("TOTAL ENERGY: "+ rounded, 25, 725);
+  return result
+}
+
+
+
+
+
+
+
+
+//start program
+let m = document.getElementById("canvas").getContext('2d')
+let friction = 0.005
+let elasticity = 0.99
+let BALLS = []
+BALLS.push(new Circle(400, 400, 25, "blue"))
+BALLS.push(new Circle(300, 400, 25, "green"))
+BALLS.push(new Circle(700, 400, 25, "orange"))
+BALLS.push(new Circle(500, 500, 25, "yellow"))
+BALLS[0].player = true
+let hud = new Circle(700, 700, 30, "white")
+
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++)
+    BALLS.push(new Circle(i*100+100, j*100+100, 25, "pink"))
+}
+
+
+//keyboard controls
+let leftHeld = false
+let rightHeld = false
+let upHeld = false
+let downHeld = false
+
+/*
+//vehicle controls
+FUTURE CODE
+let steering = 0 //-1 is left 0 is center 1 is right
+let throttle = 0 //0 is coasting, 1 is full throttle
+let brakes = 0 //0 is no brakes, 1 is full brakes
+*/
+let horizontal = 0 // -1 is left 1 is right
+let vertical = 0 // -1 is up 1 is down
+
+
+
+
 
 //physics loop
 function update() {
@@ -179,11 +250,28 @@ function update() {
 
     //draw frame
     InputHandler()
-    c1.simulate()
+
+
+
+    //c1.debug()
     //c2.simulate()
-    c1.drawCircle()
-    //c2.drawCircle()
+
+
+    BALLS.forEach((b, i) => {
+      if (b.player) { b.user_input() }
+      b.simulate()
+      b.drawCircle()
+      for (let j = i+1; j < BALLS.length; j++) {
+        if (collision_detection(BALLS[i], BALLS[j])) {
+          penetration(BALLS[i], BALLS[j])
+          response(BALLS[i], BALLS[j])
+        }
+      }
+    })
+
     Hud()
+    TotalEnergy()
+
     //draw to the screen
     requestAnimationFrame(update)
 }
